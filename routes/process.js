e2943cd1d62b39c26cee2d332afec51518e063fb
边维东 <with007@qq.com>
2016-04-24 23:10:09 +0800

    文件上传功能修改文件名+后缀名

--- routes/process.js
+++ routes/process.js
@@ -3,7 +3,20 @@
     base on async and promise
 */
 var multer = require('multer')
-var upload = multer({ dest: 'uploads/123/' })
+var storage = multer.diskStorage({
+	//设置上传后文件路径，uploads文件夹会自动创建。
+	destination: function (req, file, cb) {
+		cb(null, './uploads/123/')
+	}, 
+	//给上传文件重命名，获取添加后缀名
+	filename: function (req, file, cb) {
+		var fileFormat = (file.originalname).split(".");
+		cb(null, file.fieldname + '-' + Date.now() + "." + fileFormat[fileFormat.length - 1]);
+	}
+});
+var upload = multer({
+	storage: storage
+});
 var cpUpload = upload.any();
 var async = require('async');
 //async 是一个异步控制的模块 generator原理
