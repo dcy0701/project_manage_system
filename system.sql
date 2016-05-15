b5fe9953fa806e89ecf09e4b9bc6b33c720dd38c
dongchongyang <dongchongyang@wecash-mail.com>
2016-05-15 23:07:22 +0800

    加入前端页面&&加入服务商管理页面

--- system.sql
+++ system.sql
@@ -31,7 +31,7 @@ CREATE TABLE `golocation` (
   `photo_url` varchar(255) DEFAULT NULL,
   `flag` int(11) DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=170 DEFAULT CHARSET=utf8 COMMENT='管理表';
+) ENGINE=InnoDB AUTO_INCREMENT=172 DEFAULT CHARSET=utf8 COMMENT='管理表';
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -40,7 +40,7 @@ CREATE TABLE `golocation` (
 
 LOCK TABLES `golocation` WRITE;
 /*!40000 ALTER TABLE `golocation` DISABLE KEYS */;
-INSERT INTO `golocation` VALUES (168,'1463228861040','dcy','北交','37.33468587$-122.02357768','./uploads/dcy-1463228861040.jpg',1),(169,'1463228868317','dcy','北交','37.33468587$-122.02357768','./uploads/dcy-1463228868317.jpg',1);
+INSERT INTO `golocation` VALUES (168,'1463228861040','dcy','北交','37.33468587$-122.02357768','./uploads/dcy-1463228861040.jpg',1),(169,'1463228868317','dcy','北交','37.33468587$-122.02357768','./uploads/dcy-1463228868317.jpg',1),(170,'1463290953254','dcy','北交','39.96191650655901$116.3499685927073','./uploads/dcy-1463290953254.jpg',1),(171,'1463293175855','dcy','北交','39.96193755315208$116.3499418319018','./uploads/dcy-1463293175855.jpg',1);
 /*!40000 ALTER TABLE `golocation` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -55,8 +55,9 @@ CREATE TABLE `project_apply` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `project_check_id` varchar(11) DEFAULT NULL COMMENT '检查项id',
   `user_id` varchar(20) DEFAULT NULL COMMENT '用户id',
+  `time` varchar(30) DEFAULT NULL COMMENT '申请时间',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=76 DEFAULT CHARSET=utf8 COMMENT='信息表';
+) ENGINE=InnoDB AUTO_INCREMENT=80 DEFAULT CHARSET=utf8 COMMENT='信息表';
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -65,7 +66,7 @@ CREATE TABLE `project_apply` (
 
 LOCK TABLES `project_apply` WRITE;
 /*!40000 ALTER TABLE `project_apply` DISABLE KEYS */;
-INSERT INTO `project_apply` VALUES (1,'3','dcy'),(2,'0','dcy'),(3,'0','dcy'),(4,'0','dcy'),(5,'0','dcy'),(6,'0','dcy'),(7,'0','dcy'),(8,'4','dcy'),(9,'4','dcy'),(10,'0','dcy'),(11,'0','dcy'),(12,'0','dcy'),(13,'3','dcy'),(14,'3','dcy'),(15,'0','dcy'),(16,'0','dcy'),(17,'0','dcy'),(18,'0','dcy'),(19,'3','dcy'),(20,'3','dcy'),(21,'3','dcy'),(22,'3','dcy'),(23,'0','dcy'),(24,'3','dcy'),(25,'3','dcy'),(26,'0','dcy'),(27,'0','dcy'),(28,'2','dcy'),(29,'2','dcy'),(30,'2','dcy'),(31,'1','dcy'),(32,'3','dcy'),(33,'0','dcy'),(34,'0','dcy'),(35,'0','dcy'),(36,'2','dcy'),(37,'0','dcy'),(38,'0','dcy'),(39,'0','dcy'),(40,'0','dcy'),(41,'3','dcy'),(42,'2','dcy'),(43,'0','dcy'),(44,'0','dcy'),(45,'2','dcy'),(46,'0','dcy'),(47,'0','dcy'),(48,'2','dcy'),(49,'2','dcy'),(50,'0','dcy'),(51,'0','dcy'),(52,'0','dcy'),(53,'0','dcy'),(54,'0','dcy'),(55,'0','dcy'),(56,'0','dcy'),(57,'0','dcy'),(58,'2','dcy'),(59,'北交','dcy'),(60,'北交','dcy'),(61,'北交','dcy'),(62,'北交','dcy'),(63,'北交','dcy'),(64,'北交','dcy'),(65,'北交','dcy'),(66,'北交','dcy'),(67,'北交','dcy'),(68,'北交','dcy'),(69,'北交','dcy'),(70,'北交','dcy'),(71,'北交','dcy'),(72,'北交','dcy'),(73,'北交','dcy'),(74,'北交','dcy'),(75,'北交','dcy');
+INSERT INTO `project_apply` VALUES (1,'北邮','dcy','12273837738'),(2,'北交','dcy','21122772626'),(3,'北师','dcy','22222222222'),(4,'清华','dcy','33333333333');
 /*!40000 ALTER TABLE `project_apply` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -92,7 +93,7 @@ CREATE TABLE `project_check_info` (
 
 LOCK TABLES `project_check_info` WRITE;
 /*!40000 ALTER TABLE `project_check_info` DISABLE KEYS */;
-INSERT INTO `project_check_info` VALUES (1,'123','dcy','null','222');
+INSERT INTO `project_check_info` VALUES (1,'北邮','dcy','null','222');
 /*!40000 ALTER TABLE `project_check_info` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -230,4 +231,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2016-05-14 20:29:09
+-- Dump completed on 2016-05-15 23:07:03
