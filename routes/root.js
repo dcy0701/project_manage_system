c4b1f43e63a8155f0bb794cad73834ddbb648564
dongchongyang <dongchongyang@wecash-mail.com>
2016-06-07 17:05:58 +0800

    update

---
+++ routes/root.js
@@ -0,0 +1,239 @@
+﻿var express = require('express');
+var rootRouter = express.Router();
+var sqlConfig = require('./../mysqlConfig');
+
+var mysql = require('mysql');
+console.log(sqlConfig.database);
+var connection = mysql.createConnection({
+	host: sqlConfig.host,
+	port: sqlConfig.port,
+	user: sqlConfig.user,
+	password: sqlConfig.password,
+	databse: sqlConfig.database
+});
+connection.connect();
+
+connection.query('use ' + sqlConfig.database);
+/* GET home page. */
+rootRouter.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+var multer = require('multer')
+var storage = multer.diskStorage({
+	//设置上传后文件路径，uploads文件夹会自动创建。
+	destination: function (req, file, cb) {
+		cb(null, './uploads/progress/default/')
+	}, 
+	//给上传文件重命名，获取添加后缀名
+	filename: function (req, file, cb) {
+		var fileFormat = (file.originalname).split(".");
+		cb(null, file.fieldname + '-' + Date.now() + "." + fileFormat[fileFormat.length - 1]);
+	}
+});
+var upload = multer({
+	storage: storage
+});
+var cpUpload = upload.any();
+rootRouter.use(cpUpload);
+
+rootRouter.get('/searchuser',function (req, res, next) {
+  	var url_info = require('url').parse(req.url, true);
+    
+    var k = Object.keys(url_info.query);
+    if(k[i]=='id')
+      var temp = k[0] + '=' + url_info.query[k[0]];
+    else
+      var temp = k[0] + '="' + url_info.query[k[0]]+'"';
+
+    for (var i = 1; i < k.length; i++) {
+      if(k[i]=='id'){
+        temp += ' and ' + k[i] + '=' + url_info.query[k[i]];
+      }
+      else{
+        temp += ' and ' + k[i] + '="' + url_info.query[k[i]]+'"';
+      }
+    }
+    console.log('SELECT * FROM user where ' + temp);
+    connection.query('SELECT * FROM user where ' + temp,
+          function selectCb(err, results, fields) {
+      if (err) {
+        console.log(err);
+      }
+      
+      else if (results) {
+        if(results.length>0)
+          res.send({results:results});
+        }
+      }
+    )
+})
+
+rootRouter.post('/adduser', function (req, res) {
+	connection.query('insert into user (user_name,password,area,role) values ( "' 
+                                                                                    + req.body.user_name + '","' 
+                                                                                    + req.body.password + '","' 
+                                                                                    + req.body.area + '","' 
+                                                                                    + req.body.role + '")',
+        function selectCb(err, results, fields) {
+		if (err) {
+			console.log(err);
+			res.send(err.code);
+		} 
+		
+		if (results) {
+			res.send('添加成功');
+		}
+	}
+	);
+
+});
+
+rootRouter.get('/deleteuser', function (req, res) {
+  	var url_info = require('url').parse(req.url, true);
+    
+    var k = Object.keys(url_info.query);
+    if(k[i]=='id')
+      var temp = k[0] + '=' + url_info.query[k[0]];
+    else
+      var temp = k[0] + '="' + url_info.query[k[0]]+'"';
+
+    for (var i = 1; i < k.length; i++) {
+      if(k[i]=='id'){
+        temp += ' and ' + k[i] + '=' + url_info.query[k[i]];
+      }
+      else{
+        temp += ' and ' + k[i] + '="' + url_info.query[k[i]]+'"';
+      }
+    }
+	
+	connection.query('delete FROM user where ' + temp,
+        function selectCb(err, results, fields) {
+		if (err) {
+			console.log(err.code);
+		}
+		
+		else if (results) {
+			res.send(results.affectedRows+' records has been deleted');
+			//由results.affectedRows可以知道删除了多少行
+		}
+		
+	}
+	);
+
+});
+
+rootRouter.post('/updateuser', function (req, res) {
+
+	connection.query('update user set user_name="'     + req.body.user_name +  
+									           		'",password="'       + req.body.password +  
+										                 '",area="'      + req.body.area +
+											              '",role="'       + req.body.role +
+                                    '" where id='    + req.body.id,
+        function selectCb(err, results, fields) {
+		if (err) {
+			console.log(err);
+		}
+		
+		else if (results) {
+			if(results.affectedRows==0){
+				res.send('no match record');
+			}
+			else{
+				res.send(results.affectedRows+' records has been changed');
+			}
+		}
+	}
+	);
+});
+
+rootRouter.get('/searchrole',function (req, res, next) {
+  	var url_info = require('url').parse(req.url, true);
+    
+    var k = Object.keys(url_info.query);
+    var temp = k[0] + '="' + url_info.query[k[0]]+'"';
+
+    for (var i = 1; i < k.length; i++) 
+        temp += ' and ' + k[i] + '="' + url_info.query[k[i]]+'"';
+
+    connection.query('SELECT * FROM root_role where ' + temp,
+          function selectCb(err, results, fields) {
+      if (err) {
+        console.log(err);
+      }
+      
+      else if (results) {
+        if(results.length>0)
+          res.send({results:results});
+        }
+      }
+    )
+})
+
+rootRouter.post('/addrole', function (req, res) {
+	connection.query('insert into root_role values ( "' 
+                                            + req.body.role + '","' 
+                                            + req.body.root + '","' 
+                                            + req.body.leader + '")',
+        function selectCb(err, results, fields) {
+		if (err) {
+			console.log(err);
+			res.send(err.code);
+		} 
+		
+		if (results) {
+			res.send('添加成功');
+		}
+	}
+	);
+
+});
+
+rootRouter.get('/deleterole', function (req, res) {
+  	var url_info = require('url').parse(req.url, true);
+    
+    var k = Object.keys(url_info.query);
+    var temp = k[0] + '="' + url_info.query[k[0]]+'"';
+
+    for (var i = 1; i < k.length; i++) 
+        temp += ' and ' + k[i] + '="' + url_info.query[k[i]]+'"';
+	
+	connection.query('delete FROM root_role where ' + temp,
+        function selectCb(err, results, fields) {
+		if (err) {
+			console.log(err.code);
+		}
+		
+		else if (results) {
+			res.send(results.affectedRows+' records has been deleted');
+			//由results.affectedRows可以知道删除了多少行
+		}
+		
+	}
+	);
+
+});
+
+rootRouter.post('/updaterole', function (req, res) {
+
+	connection.query('update root_role set root="'     + req.body.root +  
+									           		'",leader="'       + req.body.leader +  
+                              '" where role="'    + req.body.role +'"',
+        function selectCb(err, results, fields) {
+		if (err) {
+			console.log(err);
+		}
+		
+		else if (results) {
+			if(results.affectedRows==0){
+				res.send('no match record');
+			}
+			else{
+				res.send(results.affectedRows+' records has been changed');
+			}
+		}
+	}
+	);
+});
+
+module.exports = rootRouter;
---
+++ uploads/web/phone/js/comment.js
@@ -0,0 +1,140 @@
+﻿
+	//$("#roleControll").hide();
+	$(function(){
+   var vars = [], hash;
+
+   var q = document.URL.split('?')[1];
+
+   if(q != undefined){
+
+       q = q.split('&');
+
+       for(var i = 0; i < q.length; i++){
+          	
+
+           hash = q[i].split('=');
+
+           vars.push(hash[1]);
+
+           vars[hash[0]] = hash[1];
+
+       }
+
+    }
+
+    var username=vars[0];
+    var area=decodeURI(vars[1]);
+    var role=decodeURI(vars[2]);
+    
+    //var user=document.getElementById("user");
+    //user[0].innerHTML=username;
+    $("u").html(username);
+
+  
+    document.getElementById('addBtn').style.display="none";
+    document.getElementById('delBtn').style.display="none";
+    document.getElementById('updBtn').style.display="none";
+
+   var host =fetch('../../r/searchrole?role='+role).then(function(data){
+			return data.json();
+			console.log(data);
+		}).then(function(json){
+			console.log(json);
+			if(json[0]!="超级管理员")
+				$("#roleControll").hide();
+			for (var i in json[1]) {
+				//json[i].time1 = String(new Date(parseInt(json[i].time))).slice(4,21);
+
+				switch(json[i]){
+					case 'A': 
+					case 'B':
+					case 'C':
+					case 'D':
+					     document.getElementById('addBtn').style.display="block";
+					case 'E':
+					    document.getElementById('delBtn').style.display="block";
+					case 'F':
+					     document.getElementById('updBtn').style.display="block";
+					case 'G':
+					case 'H':
+					case 'I':
+					case 'J':
+				}
+			}
+		})
+	});
+
+  function xinxi(){
+  	var q = document.URL.split('?')[1];
+    window.location.href='xinxi.html?'+q;
+  }
+
+  function jindu(){
+  	var q = document.URL.split('?')[1];
+    window.location.href='selectproject.html?'+q;
+  }
+
+  function zhiliang(){
+  	var q = document.URL.split('?')[1];
+    window.location.href='zhiliang.html?'+q;
+  }
+
+
+  function fuwushang(){
+  	var q = document.URL.split('?')[1];
+    window.location.href='fuwushang.html?'+q;
+  }
+
+  function xiaoguo(){
+  	var q = document.URL.split('?')[1];
+    window.location.href='xiaoguo.html?'+q;
+  }
+
+  function index(){
+  	var q = document.URL.split('?')[1];
+    window.location.href='index.html?'+q;
+  }
+
+  function user(){
+  	var q = document.URL.split('?')[1];
+    window.location.href='usermodel _delete.html?'+q;
+  }
+
+function role(){
+  	var q = document.URL.split('?')[1];
+    window.location.href='role.html?'+q;
+  }
+
+
+
+			/*var html = template('test', {"list":json});
+			document.getElementById('app').innerHTML = html;
+			$('button[sid]').click(function(){
+				// var index = $(this).attr('sid');
+				console.log(111);
+				var index = $(this).attr('sid');
+				var st = $(this).attr('st');
+				var currentline = json[index];
+				console.log(currentline);
+				fetch(`../../s/updateChargeman?user=${currentline.user_id}&project_id=${currentline.project_check_id}&flag=1&st=${st}`).then(function(data){
+					return;
+					console.log(data);
+				})
+			});
+			$('button[hid]').click(function(){
+				// var index = $(this).attr('sid');
+				console.log(222);
+				var index = $(this).attr('hid');
+				var currentline = json[index];
+				console.log(currentline);
+				var $this = $(this);
+				fetch(`../../s/updateChargeman?user=${currentline.user_id}&project_id=${currentline.project_check_id}&flag=0&st=${st}`).then(function(data){
+					$this.hide();
+					return;
+					console.log(data);
+				})
+
+			});
+		})
+
+	});*/
